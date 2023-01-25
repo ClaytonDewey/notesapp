4b870b6ba2a161e872084039619342158a109dfd
ClaytonDewey <clay@dryadmedia.com>
2023-01-25 15:34:19 -0600

    Subscribe to real-time events

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   createNote as CreateNote,
   deleteNote as DeleteNote,
 } from './graphql/mutations';
+import { onCreateNote } from './graphql/subscriptions';
 
 const CLIENT_ID = uuid();
 
@@ -112,6 +113,16 @@ const App = () => {
 
   useEffect(() => {
     fetchNotes();
+    const subscription = API.graphql({
+      query: onCreateNote,
+    }).subscribe({
+      next: (noteData) => {
+        const note = noteData.value.data.onCreateNote;
+        if (CLIENT_ID === note.clientId) return;
+        dispatch({ type: 'ADD_NOTE', note });
+      },
+    });
+    return () => subscription.unsubscribe();
   }, []);
 
   const renderItem = (item) => {
